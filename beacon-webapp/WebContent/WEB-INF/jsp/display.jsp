6f6888e1b1e8f8c0f915a694a470a4488e546761
Eleja <>
2020-01-14 09:03:42 +0800

    Sample Spring MVC web application for Module24

---
+++ beacon-webapp/WebContent/WEB-INF/jsp/display.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Display Details</title>
+</head>
+<body style="background-color:skyblue">
+
+	<h1>
+		${yourName}
+	</h1>
+	<h2>
+		${yourAge}
+	</h2>
+
+</body>
+</html>
---
+++ beacon-webapp/src/com/beacon/spring/controller/BeanController.java
@@ -0,0 +1,35 @@
+package com.beacon.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BeanController {
+
+	@RequestMapping("/")
+	public String getWelcomePage() {
+		return "index";
+	}
+	
+	@RequestMapping("/hello")
+	public ModelAndView sayHelloWorld() {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.addObject("message", "Hello World!");
+		mv.setViewName("hello");
+		
+		return mv;
+	}
+	
+	@RequestMapping("/display")
+	public String displayDetails(Model model) {
+		
+		model.addAttribute("yourName", "I'm Lej.");
+		model.addAttribute("yourAge", "18 yrs old!");
+		
+		return "display";
+		
+	}
+}
